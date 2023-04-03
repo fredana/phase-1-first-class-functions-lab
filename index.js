9c8957537430a0045dbd45f8f55158525b0e30c3
freda <ndyfreda@gmail.com>
2023-04-02 20:14:27 -0400

    first class function lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = (value) => {
+    return value.slice(0, 2);
+}
+
+const returnLastTwoDrivers = (value) => {
+    return value.slice(2, 4);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = (value) => {
+    return function fareMultiplier(value2){
+        return value*value2;
+    };
+}
+
+const fareDoubler = (value) => {
+return value*2;
+}
+
+const fareTripler = (value) => {
+    return value*3;
+}
+
+function selectDifferentDrivers(array,value){
+    if (value === returnFirstTwoDrivers){
+        return ['Antonia', 'Nuru'];
+    }else if (value === returnLastTwoDrivers){
+        return ['Amari', 'Mo'];
+    }
+    
+} 
